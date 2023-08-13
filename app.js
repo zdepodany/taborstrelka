2d48bc3ac5148026f4f20dc00ec24494a8a807a9
Zdenek Podany <zdepodany@gmail.com>
2023-08-14 01:15:37 +0200

    change express port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { dirname } from 'path';
 import nodemailer from 'nodemailer';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
