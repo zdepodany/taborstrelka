d83a523ec959394996f48fe551c49d793077b83b
Zdenek Podany <zdepodany@gmail.com>
2023-10-20 15:21:38 +0200

    Change email service, add captcha
    Change email service provider to gmail - zdepodanywork@gmail.com
    Add Google reCAPTCHA v3 - under maintaince of zdepodany@gmail.com

--- mailTransporter.js
+++ mailTransporter.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 //Configure connection to mailclient
 const transporter = nodemailer.createTransport({
-    host: 'smtp.seznam.cz',
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
