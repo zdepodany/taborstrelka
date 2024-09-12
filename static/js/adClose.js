5190efbbc9c53f98b2d07de666fb25a34631ad72
Zdenek Podany <zdepodany@gmail.com>
2024-09-13 00:51:31 +0200

    Change files for Github Pages

--- static/js/adClose.js
+++
@@ -1,6 +0,0 @@
-const closeBtn = document.getElementById(`adCloseBtn`);
-const ad = document.getElementById(`igAd`);
-
-closeBtn.addEventListener(`click`, () => {
-    ad.style.display = `none`;
-});
